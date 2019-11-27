f51860e741705c66ff462de517900ad638ef60d6
jupiter <jsjupiter@itcom888.com>
2019-11-27 09:14:47 +0800

    chore: add vs code prettier config

---
+++ .prettierrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    eslintIntegration: true,
+    trailingComma: 'es5',
+    tabWidth: 4,
+    printWidth: 80,
+    semi: false,
+    singleQuote: true,
+    htmlWhitespaceSensitivity: 'ignore',
+    endOfLine: 'lf',
+}
